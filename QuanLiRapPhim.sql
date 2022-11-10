d99b42197c6d49ad4f301a119900e4ccec8a610c
dongquocthanh <thanhdqit26@gmail.com>
2022-11-10 09:41:24 +0700

    Sửa sql mới
    Bổ sung các ràng buộc giúp dữ liệu chặt chẽ hơn

--- QuanLiRapPhim.sql
+++ QuanLiRapPhim.sql
@@ -1,104 +1,124 @@
-create database duan
+﻿create database Duan1_QLRP
 go
-use  duan1
+use  Duan1_QLRP
 go
+
+--ham tao khoa chinh tu dong theo mau
+CREATE FUNCTION AUTO_IDNV()
+RETURNS VARCHAR(5)
+AS
+BEGIN
+	DECLARE @ID VARCHAR(5)
+	IF (SELECT COUNT(MaNV) FROM NhanVien) = 0
+		SET @ID = '0'
+	ELSE
+		SELECT @ID = MAX(RIGHT(MaNV, 3)) FROM NhanVien
+		SELECT @ID = CASE
+			WHEN @ID >= 0 and @ID < 9 THEN 'NV00' + CONVERT(CHAR, CONVERT(INT, @ID) + 1)
+			WHEN @ID >= 9 THEN 'NV0' + CONVERT(CHAR, CONVERT(INT, @ID) + 1)
+		END
+	RETURN @ID
+END
+
+--bang nhan vien
 create table NhanVien(
-	MaNV nvarchar(10) , /* Pk */
-	HoTen nvarchar(100),
-	GioiTinh bit,
-	NgaySinh date,
-	NgayVaoLam date,
-	ChucVu nvarchar(100),
-	Luong int,
+	MaNV nvarchar(10) primary key constraint idnv default dbo.AUTO_IDNV(), /* Pk */
+	HoTen nvarchar(100) not null,
+	GioiTinh bit not null default 1,
+	NgaySinh date not null,
+	NgayVaoLam date not null DEFAULT getdate(),
+	ChucVu nvarchar(100) not null,
+	Luong int not null check (luong>0),
 	Hinh nvarchar(100),
 	GhiChu nvarchar(200),
-	MatKhau nvarchar(50)
-	primary key (MaNV)
-	 
+	MatKhau nvarchar(50) not null
 )
+
+--bang phim
 create table Phim(
-	MaPhim nvarchar(10),	/* PK */
-	TenPhim nvarchar(200),
-	TheLoai nvarchar(100),
-	ThoiLuong int,
+	MaPhim nvarchar(10) primary key not null,	/* PK */
+	TenPhim nvarchar(200) not null,
+	TheLoai nvarchar(100) not null,
+	ThoiLuong int not null check(thoiluong >0),
 	QuocGia nvarchar(50),
 	Hinh nvarchar(100),
 	GhiChu nvarchar(200)
-	primary key (MaPhim)
+)
 
-)
-create table HoaDon(
-	MaHD int IDENTITY(1,1),  /* Pk */
-	MaNV nvarchar(10), /* Fk */
-	NgayLap date
-	primary key (MAHD),
-	FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV)
-)
+-- bang thuc don
 create table ThucDon(
-	MaMon nvarchar(10), /* Pk */
-	TenMon nvarchar(200), 
-	DonGia Float,
+	MaMon int IDENTITY(1,1) primary key, /* Pk */
+	TenMon nvarchar(200) not null, 
+	DonGia Float not null check(DonGia >0),
 	DonViTinh nvarchar(50),
-	Loai nvarchar(50),
+	Loai nvarchar(50) not null,
 	Hinh nvarchar(100),
-	GhiChu nvarchar(200)
-	primary key (MaMon)
-	
-)
-create table PhongChieu(
-	MaPhong nvarchar(10), /* Pk */
-	SoluongGhe int,
-	DienTich nvarchar(50),
-	MayChieu nvarchar(50),
-	AmThanh nvarchar(50),
-	TinhTrang nvarchar(200),
 	GhiChu nvarchar(200),
-	primary key (MaPhong)
 )
 
+--bang phong chieu
+create table PhongChieu(
+	MaPhong nvarchar(10) primary key not null, /* Pk */
+	SoluongGhe int not null,
+	DienTich nvarchar(50) not null,
+	MayChieu nvarchar(50) not null,
+	AmThanh nvarchar(50) not null,
+	TinhTrang nvarchar(200) default N'Mới',
+	GhiChu nvarchar(200),
+	check(SoluongGhe > 0 and DienTich > 0)
+)
+
+--bang ghe
 create table Ghe(
-	MaPhong nvarchar (10), /* Fk  */
-	MaGhe nvarchar(10), /*  PK */
-	Gia Float,
-	primary key (MaGhe,MaPhong),
-	FOREIGN KEY (MaPhong) REFERENCES PhongChieu(MaPhong)
-
-
-	
+	MaPhong nvarchar (10) not null, /* Fk  */
+	MaGhe nvarchar (10) not null, /*  PK */
+	Gia Float not null default 10000.0,
+	constraint pk_gh primary key (MaGhe,MaPhong),
+	constraint fk_gh_ph FOREIGN KEY (MaPhong) REFERENCES PhongChieu(MaPhong) on delete cascade
 )
+
+--bang hoa don
+create table HoaDon(
+	MaHD int IDENTITY(1,1) primary key,  /* Pk */
+	MaNV nvarchar(10) not null, /* Fk */
+	NgayLap date not null DEFAULT getdate(),
+	constraint fk_hd_nv FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV) on update cascade
+)
+
+--bang hoa don ct
 create table HoaDonCT(
-	MaHD int identity(1,1), /* PK Fk */
-	MaMon nvarchar(10), /* PK Fk */
-	SoLuong int
-	primary key (MaHD,MaMon),
-	FOREIGN KEY (MaMon) REFERENCES Thucdon(MaMon),
-	FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD)
+	MaHD int identity(1,1) not null, /* PK Fk */
+	MaMon int not null, /* PK Fk */
+	SoLuong int not null
+	constraint pk_hdct primary key (MaHD,MaMon),
+	constraint fk_hdct_td FOREIGN KEY (MaMon) REFERENCES Thucdon(MaMon),
+	constraint fk_hdct_hd FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD),
+	check(Soluong > 0)
 )
 
+--bang lich chieu
 create table LichChieu(
-	MaLichChieu nvarchar(20), /* PK */
-	MaPhim nvarchar(10),	/* Fk */
-	GioChieu int,
-	NgayChieu date,
-	MaPhong nvarchar(10) /* Fk */
-	primary key (MaLichChieu),
-	FOREIGN KEY (MaPhim) REFERENCES Phim(MaPhim),
-	FOREIGN KEY (MaPhong) REFERENCES PhongChieu(MaPhong)
+	MaLichChieu nvarchar(20) primary key not null, /* PK */
+	MaPhim nvarchar(10) not null,	/* Fk */
+	GioChieu int not null,
+	NgayChieu date not null DEFAULT getdate(),
+	MaPhong nvarchar(10) not null/* Fk */
+	constraint fk_lc_ph FOREIGN KEY (MaPhim) REFERENCES Phim(MaPhim) on update cascade,
+	constraint fk_lc_pc FOREIGN KEY (MaPhong) REFERENCES PhongChieu(MaPhong) on update cascade,
+	check(GioChieu >= 0 and GioChieu < 24)
 )
+
+--bang ve
 create table Ve(
-	MaVe nvarchar(10), /* Pk */
-	MaNV nvarchar(10), /* Fk */
-	NgayLap date,
-	MaGhe nvarchar(10), /* Fk */
-	MaPhong nvarchar(10), /* Fk */
-	MaPhim nvarchar(10), /* Fk */
-	primary key (MaVe),
-	FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV),	
-	FOREIGN KEY (MaPhong) REFERENCES PhongChieu(MaPhong),
-	FOREIGN KEY (MaPhim) REFERENCES Phim(MaPhim)
-
+	MaVe nvarchar(10) primary key not null, /* Pk */
+	MaNV nvarchar(10) not null, /* Fk */
+	NgayLap date not null  DEFAULT getdate(),
+	MaGhe nvarchar(10) not null, /* Fk */
+	MaPhong nvarchar(10) not null, /* Fk */
+	MaPhim nvarchar(10) not null, /* Fk */
+	constraint fk_ve_nv FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV) on update cascade,	
+	constraint fk_ve_gh FOREIGN KEY (MaGhe, MaPhong) REFERENCES Ghe(MaGhe, MaPhong) on update cascade,
+	constraint fk_ve_ph FOREIGN KEY (MaPhim) REFERENCES Phim(MaPhim) on update cascade
 )
-
-
 
 
