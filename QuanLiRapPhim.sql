b21fc26cc0f44a10f0ec48b71ca187292451435d
huyquang123 <62505347+huyhocmon12@users.noreply.github.com>
2022-11-14 16:47:07 +0700

    (SQL)

--- QuanLiRapPhim.sql
+++ QuanLiRapPhim.sql
@@ -4,34 +4,60 @@ use  Duan1_QLRP
 go
 
 --ham tao khoa chinh tu dong theo mau
-CREATE FUNCTION AUTO_IDNV()
+CREATE FUNCTION AUTO_IDUS()
 RETURNS VARCHAR(5)
 AS
 BEGIN
 	DECLARE @ID VARCHAR(5)
-	IF (SELECT COUNT(MaNV) FROM NhanVien) = 0
+	IF (SELECT COUNT(MaUser) FROM Users) = 0
 		SET @ID = '0'
 	ELSE
-		SELECT @ID = MAX(RIGHT(MaNV, 3)) FROM NhanVien
+		SELECT @ID = MAX(RIGHT(MaUser, 3)) FROM Users
 		SELECT @ID = CASE
-			WHEN @ID >= 0 and @ID < 9 THEN 'NV00' + CONVERT(CHAR, CONVERT(INT, @ID) + 1)
-			WHEN @ID >= 9 THEN 'NV0' + CONVERT(CHAR, CONVERT(INT, @ID) + 1)
+			WHEN @ID >= 0 and @ID < 9 THEN 'US00' + CONVERT(CHAR, CONVERT(INT, @ID) + 1)
+			WHEN @ID >= 9 THEN 'US0' + CONVERT(CHAR, CONVERT(INT, @ID) + 1)
 		END
 	RETURN @ID
 END
 
+CREATE FUNCTION AUTO_IDMV()
+RETURNS VARCHAR(5)
+AS
+BEGIN
+	DECLARE @ID VARCHAR(5)
+	IF (SELECT COUNT(MaVe) FROM Ve) = 0
+		SET @ID = '0'
+	ELSE
+		SELECT @ID = MAX(RIGHT(MaVe, 3)) FROM Ve
+		SELECT @ID = CASE
+			WHEN @ID >= 0 and @ID < 9 THEN 'MV00' + CONVERT(CHAR, CONVERT(INT, @ID) + 1)
+			WHEN @ID >= 9 THEN 'MV0' + CONVERT(CHAR, CONVERT(INT, @ID) + 1)
+		END
+	RETURN @ID
+END
+
+
 --bang nhan vien
-create table NhanVien(
-	MaNV nvarchar(10) primary key constraint idnv default dbo.AUTO_IDNV(), /* Pk */
+create table Users(
+	MaUser nvarchar(10) primary key constraint idnv default dbo.AUTO_IDUS(), /* Pk */
 	HoTen nvarchar(100) not null,
+	SoDT nvarchar(10),
+	Email nvarchar(30),
 	GioiTinh bit not null default 1,
 	NgaySinh date not null,
 	NgayVaoLam date not null DEFAULT getdate(),
-	ChucVu nvarchar(100) not null,
-	Luong int not null check (luong>0),
+	MaRole int not null,
+	Luong int null check (luong>0),
 	Hinh nvarchar(100),
 	GhiChu nvarchar(200),
-	MatKhau nvarchar(50)
+	MatKhau nvarchar(50)not null
+	constraint fk_rl_us FOREIGN KEY (MaRole) REFERENCES Role(MaRole) on update cascade
+)
+
+create table Role(
+	MaRole int primary key,
+	VaiTro nvarchar(100)
+
 )
 
 --bang phim
@@ -80,9 +106,9 @@ create table Ghe(
 --bang hoa don
 create table HoaDon(
 	MaHD int IDENTITY(1,1) primary key,  /* Pk */
-	MaNV nvarchar(10) not null, /* Fk */
+	MaUser nvarchar(10) not null, /* Fk */
 	NgayLap date not null DEFAULT getdate(),
-	constraint fk_hd_nv FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV) on update cascade
+	constraint fk_hd_us FOREIGN KEY (MaUser) REFERENCES Users(MaUser) on update cascade
 )
 
 --bang hoa don ct
@@ -110,49 +136,82 @@ create table LichChieu(
 
 --bang ve
 create table Ve(
-	MaVe nvarchar(10) primary key not null, /* Pk */
-	MaNV nvarchar(10) not null, /* Fk */
+	MaVe nvarchar(10) primary key not null constraint idmv default dbo.AUTO_IDMV(), /* Pk */
+	MaUser nvarchar(10) not null, /* Fk */
 	NgayLap date not null  DEFAULT getdate(),
 	MaGhe nvarchar(10) not null, /* Fk */
 	MaPhong nvarchar(10) not null, /* Fk */
 	MaPhim nvarchar(10) not null, /* Fk */
-	constraint fk_ve_nv FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV) on update cascade,	
+	constraint fk_ve_nv FOREIGN KEY (MaUser) REFERENCES Users(MaUser) on update cascade,	
 	constraint fk_ve_gh FOREIGN KEY (MaGhe, MaPhong) REFERENCES Ghe(MaGhe, MaPhong) on update cascade,
 	constraint fk_ve_ph FOREIGN KEY (MaPhim) REFERENCES Phim(MaPhim) on update cascade
 )
-
+-- Nhập dữ liệu bảng role
+insert into Role(MaRole, VaiTro) values
+(1,N'Quản lý rạp');
+insert into Role(MaRole, VaiTro) values
+(2,N'Nhân viên bán hàng');
+insert into Role(MaRole, VaiTro) values
+(3,N'Nhân viên đặt vé');
+insert into Role(MaRole, VaiTro) values
+(4,N'Nhân viên quản lý phim');
+insert into Role(MaRole, VaiTro) values
+(5,N'Nhân viên soát vé');
+insert into Role(MaRole, VaiTro) values
+(6,N'Khách hàng');
 
 -- Nhập Dữ Liệu Bảng Nhân Viên
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Hồ Văn Khánh',1,'2003-10-22','2022-05-23',N'Nhân Viên Đặt vé ',3000000,'0000');
- Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Đồng Quốc Thành',1,'2003-05-12','2021-11-12',N'Quản Lí Rạp ',6000000,'0000');
- Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Đặng Quang Huy',1,'2003-09-18','2022-07-22',N' Nhân Viên Quản lí Phim ',3000000,'0000');
- Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Trịnh Thế Qúy',1,'2003-02-13','2022-01-08',N'Nhân Viên Bán Hàng' ,3000000,'0000');
- Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Phan Diệu Linh',0,'2003-12-24','2022-03-02',N'Nhân Viên Bán Hàng',3000000,'0000');
- Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Trần Văn Hiệp',1,'2003-09-21','2022-05-23',N'Nhân Viên Soát Vé',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Phạm Ngọc Sang',1,'2003-04-15','2022-03-17',N'Nhân Viên Soát Vé',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Phạm Thị Mỹ Lệ',0,'2003-10-22','2022-03-03',N'Nhân Viên Đặt Vé',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Nguyễn Thị Hồng',0,'2002-08-17','2022-09-12',N'Nhân Viên Bán Hàng',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Nguyễn Văn Huy',1,'2003-03-22','2022-05-23',N'Nhân Viên Đặt Vé',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Phạm Thị Ái Hoanh',0,'2002-11-12','2022-08-26',N'Nhân Viên Bán Hàng',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Lê Minh Trọng',1,'2003-09-26','2022-05-06',N' Nhân Viên Quản Lí Phim',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Nguyễn Thị Kiều Diễm',0,'2003-12-04','2022-10-19',N'Nhân Viên Đặt Vé',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Hoàng Thị Diễm Phương',0,'2001-02-02','2022-10-22',N'Nhân Viên Đặt Vé',3000000,'0000');
-Insert into NhanVien(HoTen,GioiTinh,NgaySinh,NgayVaoLam,ChucVu,Luong,MatKhau) values 
-(N'Trần Đức Tiến',0,'2003-09-18','2022-09-12',N'Nhân Viên Bán Hàng',3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Hồ Văn Khánh','0123987654','mailam114@gmail.com',1,'2003-10-22','2022-05-23',3,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Đồng Quốc Thành','0987654321','tailanh766@gmail.com',1,'2003-05-12','2021-11-12',1,6000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Đặng Quang Huy','0768915158','danghuy1123@gmail.com',1,'2003-09-18','2022-07-22',4,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Trịnh Thế Qúy','0998877766','huydq112@gmail.com',1,'2003-02-13','2022-01-08',2 ,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Phan Diệu Linh','0123645798','tutu222@gmail.com',0,'2003-12-24','2022-03-02',2,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Trần Văn Hiệp','0331278644','hiepvan776@gmail.com',1,'2003-09-21','2022-05-23',5,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Phạm Ngọc Sang','1237650489','sangsang88@gmail.com',1,'2003-04-15','2022-03-17',5,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Phạm Thị Mỹ Lệ','095643218','myle193@gmail.com',0,'2003-10-22','2022-03-03',3,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Nguyễn Thị Hồng','8765912340','hongng22@gmail.com',0,'2002-08-17','2022-09-12',2,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Nguyễn Văn Huy','0674312895','huydq112@gmail.com',1,'2003-03-22','2022-05-23',3,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Phạm Thị Ái Hoanh','7658893123','aihoanh229@gmail.com',0,'2002-11-12','2022-08-26',2,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Lê Minh Trọng','0934069967','trongtt334@gmail.com',1,'2003-09-26','2022-05-06',4,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Nguyễn Thị Kiều Diễm','0765889123','diemkieu287@gmail.com',0,'2003-12-04','2022-10-19',5,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Hoàng Thị Diễm Phương','0995642231','phuongdm667@gmail.com',0,'2001-02-02','2022-10-22',5,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,Luong,MatKhau) values 
+(N'Trần Đức Tiến','0332341143','tienduc098@gmail.com',1,'2003-09-18','2022-09-12',2,3000000,'0000');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Nguyễn Thị Kim Anh','1237896450','kimanh112@gmail.com',0,'1968-12-01','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Đặng Tấn Tài','7755462087','taidang096@gmail.com',1,'1968-11-01','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Mã Tiến An','4321678095','antienma23@gmail.com',1,'1998-10-11','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Trần Văn Đại','1789450323','daitran665@gmail.com',1,'1997-04-01','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Mason Gỗ Xanh','1900567782','xanhmason760@gmail.com',0,'1999-09-13','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Quý Quét Nơ','0180033889','nono886@gmail.com',1,'1993-12-01','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Đặng Mắc Hài','2094317812','danghai774@gmail.com',1,'2001-11-17','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Ra Phen Va Ren','5578901234','renva246@gmail.com',0,'2003-12-17','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Cao Ri Đỗ','6572218903','dori434@gmail.com',1,'2003-11-16','',1,'1111');
+Insert into Users(HoTen,SoDT, Email, GioiTinh,NgaySinh,NgayVaoLam,MaRole,MatKhau) values 
+(N'Phan Sa Tị','9978065541','tisa2323@gmail.com',1,'2004-06-06','',1,'1111');
+
 
 -- Nhập Dữ Liệu Bảng Phim 
 Insert into Phim(MaPhim,TenPhim,TheLoai,ThoiLuong,QuocGia) values
@@ -246,7 +305,7 @@ Insert into PhongChieu(MaPhong,SoluongGhe,DienTich,MayChieu,AmThanh,TinhTrang) v
 Insert into PhongChieu(MaPhong,SoluongGhe,DienTich,MayChieu,AmThanh,TinhTrang) values
 (N'PC11',150,'300',N'Loại A',N'Loại B',N'Tốt');
 Insert into PhongChieu(MaPhong,SoluongGhe,DienTich,MayChieu,AmThanh,TinhTrang) values
-(N'PC10',150,'300',N'Loại A',N'Loại C',N'Khá');
+(N'PC12',150,'300',N'Loại A',N'Loại C',N'Khá');
 
 --Nhập DữLiệu Vào Bảng Ghế
 Insert into Ghe(MaPhong,MaGhe,Gia) values 
@@ -292,7 +351,7 @@ Insert into Ghe(MaPhong,MaGhe,Gia) values
 Insert into Ghe(MaPhong,MaGhe,Gia) values 
 (N'PC10',N'G3&G4',105000);
 Insert into Ghe(MaPhong,MaGhe,Gia) values 
-(N'PC09',N'E4',75000);
+(N'PC11',N'E5',75000);
 Insert into Ghe(MaPhong,MaGhe,Gia) values 
 (N'PC01',N'G1&G2',105000);
 Insert into Ghe(MaPhong,MaGhe,Gia) values 
@@ -302,66 +361,64 @@ Insert into Ghe(MaPhong,MaGhe,Gia) values
 
 
 -- Nhập Dữ Liệu Vào Bảng Hóa Đơn
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV004','2022-05-22');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV005','2022-07-21');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV009','2022-11-18');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV011','2022-04-11');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV015','2022-07-08');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV011','2022-12-04');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV004','2022-03-12');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV004','2022-03-16');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV005','2022-10-22');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV015','2022-09-02');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV009','2022-06-07');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV011','2022-11-02');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV005','2022-12-18');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV004','2022-10-08');
-Insert into HoaDon(MaNV,NgayLap) values 
-(N'NV009','2022-02-12');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US013','2022-05-22');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US008','2022-07-21');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US006','2022-11-18');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US003','2022-04-11');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US004','2022-07-08');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US012','2022-12-04');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US007','2022-03-12');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US009','2022-03-16');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US010','2022-10-22');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US011','2022-09-02');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US013','2022-06-07');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US014','2022-11-02');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US015','2022-12-18');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US002','2022-10-08');
+Insert into HoaDon(MaUser,NgayLap) values 
+(N'US001','2022-02-12');
 
 --Nhập Dữ Liệu Vào Bảng Hóa Đơn Chi Tiết
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('2','2','2');
+('18','2','2');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('4','15','1');
+('20','15','1');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('5','17','1');
+('17','17','1');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('12','16','4');
+('28','16','4');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('10','2','2');
+('21','2','2');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('9','11','2');
+('22','11','2');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('15','18','1');
+('19','18','1');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('3','10','3');
+('23','10','3');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('7','12','2');
+('25','12','2');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('14','3','1');
+('30','3','1');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('8','5','2');
+('26','5','2');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('13','8','2');
+('29','8','2');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('6','17','1');
-Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
-('15','18','1');
+('27','17','1');
 Insert into HoaDonCT(MaHD,MaMon,SoLuong) values
 ('16','2','2');
 
@@ -390,32 +447,32 @@ insert into LichChieu(MaPhim,GioChieu,NgayChieu,MaPhong)values
 
 
 --Nhập Dữ Liệu Vào Bảng Vé
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV01','NV001','2022-08-18','A2','PC04','P005');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV02','NV008','2022-08-11','A6','PC04','P015');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV014','NV010','2022-07-07','C3','PC02','P007');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV05','NV014','2022-01-22','A4','PC06','P002');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV06','NV013','2022-06-06','A6','PC08','P011');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV07','NV008','2022-11-12','F6','PC03','P006');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV08','NV001','2022-01-11','G1&G2','PC02','P008');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV09','NV001','2022-12-09','B3','PC01','P003');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV10','NV010','2022-04-30','E4','PC09','P012');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV11','NV013','2022-05-08','D6','PC01','P009');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV12','NV008','2022-09-27','G5&G6','PC05','P004');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV13','NV001','2022-12-01','C2','PC08','P005');
-insert into Ve(MaVe,MaNV,NgayLap,MaGhe,MaPhong,MaPhim)values
-('MV14','NV010','2022-09-15','G3&G4','PC03','P013');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV01','US001','2022-08-18','A2','PC04','P005');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV02','US008','2022-08-11','A6','PC04','P015');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV014','US010','2022-07-07','C3','PC02','P007');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV05','US014','2022-01-22','A4','PC06','P002');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV06','US013','2022-06-06','A6','PC08','P011');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV07','US008','2022-11-12','F6','PC03','P006');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV08','US001','2022-01-11','G1&G2','PC02','P008');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV09','US001','2022-12-09','B3','PC01','P003');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV10','US010','2022-04-30','E4','PC09','P012');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV11','US013','2022-05-08','D6','PC01','P009');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV12','US008','2022-09-27','G5&G6','PC05','P004');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV13','US001','2022-12-01','C2','PC08','P005');
+insert into Ve(MaVe,MaUser,NgayLap,MaGhe,MaPhong,MaPhim)values
+('MV14','US010','2022-09-15','G3&G4','PC03','P013');
 
 
 
